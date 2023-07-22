9544281c3edd458570cc324402afb2c2540a29c3
Osama Ibrahim <osama.ibrahim.sadek@gmail.com>
2023-07-22 19:00:28 +0300

    navDone

--- src/app/Component/home-page/slider/slider.component.ts
+++ src/app/Component/home-page/slider/slider.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+  slides = [
+    { image: '../../../../assets/Images/2.jpg' },
+    { image: '../../../../assets/Images/2.jpg' },
+    { image: '../../../../assets/Images/2.jpg' }
+    // Add more slides as needed with their respective image paths
+  ];
+
+  currentSlideIndex = 0;
+
+  ngOnInit() {
+    this.showSlide(this.currentSlideIndex);
+  }
+
+  showSlide(index: number) {
+    this.currentSlideIndex = index;
+  }
+
+  nextSlide() {
+    this.currentSlideIndex =
+      (this.currentSlideIndex + 1) % this.slides.length;
+    this.showSlide(this.currentSlideIndex);
+  }
+
+  prevSlide() {
+    this.currentSlideIndex =
+      (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
+    this.showSlide(this.currentSlideIndex);
+  }
 
 }
