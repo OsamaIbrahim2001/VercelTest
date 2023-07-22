f6147273ace52b3063b6402605c9b5a230fe3301
Osama Ibrahim <osama.ibrahim.sadek@gmail.com>
2023-07-22 20:48:11 +0300

    slider

--- src/app/Component/home-page/slider/slider.component.ts
+++ src/app/Component/home-page/slider/slider.component.ts
@@ -7,32 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
   slides = [
-    { image: '../../../../assets/Images/2.jpg' },
-    { image: '../../../../assets/Images/2.jpg' },
-    { image: '../../../../assets/Images/2.jpg' }
-    // Add more slides as needed with their respective image paths
+    { image: '../../../../assets/Images/2.jpg', alt: 'First slide' },
+    { image: '../../../../assets/Images/1.jpg', alt: 'Second slide' },
+    { image: '../../../../assets/Images/2.jpg', alt: 'Third slide' },
+    { image: '../../../../assets/Images/1.jpg', alt: 'Third slide' }
+    // Add more slides as needed with their respective image paths and alt text
   ];
 
   currentSlideIndex = 0;
+  selectedImage='';
 
   ngOnInit() {
-    this.showSlide(this.currentSlideIndex);
+    this.selectedImage=this.slides[this.currentSlideIndex].image;
   }
 
   showSlide(index: number) {
     this.currentSlideIndex = index;
+    // this.hideAllSlides();
   }
 
+  hideAllSlides() {
+    const slides = document.querySelectorAll('.carousel-item');
+    for (let i = 0; i < slides.length; i++) {
+      const slide = slides[i] as HTMLElement;
+      slide.style.opacity = '0';
+      slide.style.visibility = 'hidden';
+    }
+
+    const activeSlide = slides[this.currentSlideIndex] as HTMLElement;
+    activeSlide.style.opacity = '1';
+    activeSlide.style.visibility = 'visible';
+  }
+
+
   nextSlide() {
-    this.currentSlideIndex =
-      (this.currentSlideIndex + 1) % this.slides.length;
-    this.showSlide(this.currentSlideIndex);
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+    this.selectedImage=this.slides[this.currentSlideIndex].image;
+    // this.showSlide(this.currentSlideIndex);
   }
 
   prevSlide() {
-    this.currentSlideIndex =
-      (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
-    this.showSlide(this.currentSlideIndex);
-  }
+    this.currentSlideIndex = (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
 
+    this.selectedImage=this.slides[this.currentSlideIndex].image;
+    console.log(this.selectedImage);
+
+    // this.showSlide(this.currentSlideIndex);
+  }
 }
