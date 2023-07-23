3bdc6f7130c24b021edefdb0572e15624551f979
Osama Ibrahim <osama.ibrahim.sadek@gmail.com>
2023-07-23 17:19:08 +0300

    conacts

--- src/app/Component/home-page/equipments/equipments.component.ts
+++ src/app/Component/home-page/equipments/equipments.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentsComponent implements OnInit {
 
+  projects = [
+    // {
+    //   id: 1,
+    //   title: 'تطوير مركز فيرجينيا جامعة الاسكندرية',
+    //   description: 'تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية',
+    // },
+    {
+      id: 1,
+      title: 'تطوير مركز فيرجينيا جامعة الاسكندرية',
+      description: 'تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية',
+    },
+    {
+      id: 1,
+      title: 'تطوير مركز فيرجينيا جامعة الاسكندرية',
+      description: 'تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية',
+    },
+    {
+      id: 1,
+      title: 'تطوير مركز فيرجينيا جامعة الاسكندرية',
+      description: 'تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية',
+    },
+    {
+      id: 1,
+      title: 'تطوير مركز فيرجينيا جامعة الاسكندرية',
+      description: 'تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية',
+    },
+    {
+      id: 1,
+      title: 'تطوير مركز فيرجينيا جامعة الاسكندرية',
+      description: 'تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية تطوير مركز فيرجينيا جامعة الاسكندرية',
+    },
+    // Add more projects as needed
+  ];
+
   ngOnInit(): void {
     this.pageElement=Math.floor(this.totalCount/this.pageSize)+(this.totalCount%this.pageSize>0?1:0);
 
@@ -15,11 +49,25 @@ export class EquipmentsComponent implements OnInit {
   }
 
   selectedImage:number=0;
-  totalCount: number = 15;
+  totalCount: number = 11;
   pageNumber: number = 1;
   pageSize: number = 4;
   pageElement=0;
   buttonArray:number[]=[];
+
+  changeImage(sImage:number){
+    this.selectedImage=sImage;
+    console.log("Image ==>> ",this.selectedImage++);
+    console.log("Image ==>> ",this.selectedImage++);
+    console.log("Image ==>> ",this.selectedImage++);
+  }
+
+  resetImage(num:number){
+    this.selectedImage=0;
+    console.log("Image ==>> ",this.selectedImage);
+    console.log("Image ==>> ",num);
+  }
+
 
   previousPage() {
     if (this.pageNumber > 1) {
